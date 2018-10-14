500885b2c1226a20dea5cb71ec9f447d23a3fc82
Tom Fitzpatrick <tfitz237@gmail.com>
2018-10-14 15:11:11 -0400

    fix auth

--- src/iti/iti.module.ts
+++ src/iti/iti.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ItiController } from './iti.controller';
 import { ItiService } from './iti.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ItiController],
   providers: [ItiService]
 })
