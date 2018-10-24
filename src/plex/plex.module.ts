3cd9558004d67a3fca3b8df5236ed72ece2b62c9
Tom Fitzpatrick <tfitz237@gmail.com>
2018-10-23 21:55:58 -0400

    fix: add auth module

--- src/plex/plex.module.ts
+++ src/plex/plex.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import PlexController from './plex.controller';
 import PlexDb from './plex.db';
 import { SharedModule } from '../shared/shared.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [SharedModule],
+    imports: [AuthModule, SharedModule],
     controllers: [PlexController],
     providers: [PlexDb]
 })
