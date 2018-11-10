dc5538989a5085be1c9797589a760e016a879aca
Tom Fitzpatrick <tfitz237@gmail.com>
2018-11-10 10:02:24 -0500

    fix: async, add remove method

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards, Post, Body } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Body, Delete } from '@nestjs/common';
 import { SubscriptionsService } from './subscriptions.service';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -8,13 +8,18 @@ import { AuthGuard } from '@nestjs/passport';
 export class SubscriptionsController {
 
     constructor(private readonly subService: SubscriptionsService) {}
-    @Get('subscriptions')
-    getSubscriptions() {
-        return this.subService.getSubscriptions()
+    @Get()
+    async getSubscriptions() {
+        return await this.subService.getSubscriptions();
     }
 
-    @Post('subscriptions')
-    addSubscriptions(@Body() body: any) {
-        return this.subService.addSubscription(body.name, body.season);
+    @Post()
+    async addSubscriptions(@Body() body: any) {
+        return await this.subService.addSubscription(body.name, body.season);
+    }
+
+    @Delete()
+    async removeSubscriptions(@Body() body: any) {
+        return await this.subService.removeSubscription(body.name, body.season, body.id);
     }
 }
