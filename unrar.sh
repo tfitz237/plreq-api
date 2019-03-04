cfb361190e94fb3fd6b29695b110f4ce9eaca05c
Tom Fitzpatrick <tfitz237@gmail.com>
2019-03-04 16:09:35 -0500

    fix shell script

--- unrar.sh
+++ unrar.sh
@@ -3,10 +3,10 @@ declare -a passwords=("soitgoes" "luelinks" "intotheinternet")
 output=""
 result="Failure"
 for password in ${passwords[*]}; do
-    file=$(find "/media/large/User/Downloads/${1}" -name '*.rar' | head -1)
+    file=$(find "/media/large/User/Downloads/${1}" -name '*.rar' | sort | head -1)
     echo "Extracting $file"
-    cd $(dirname "$file")
-    output=$(unrar x -p$password $(printf %q $file))
+    cd "$(dirname "$file")""
+    output=$(unrar x -p$password "$file"))
     if [[ $output =~ .*"All OK" ]]
     then
         result="Success"
